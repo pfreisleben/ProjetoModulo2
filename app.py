9ae7adf68697b2f79444b38a044f2e8fdd64ef3a
pfreisleben <pedrofreislebeen@gmail.com>
2021-07-07 20:04:59 -0300

    ok

--- app.py
+++ app.py
@@ -1,21 +1,18 @@
-from flask import Flask, render_template, request  # Importaçao dos módulos
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
-# Configurações do banco de dados
-user = 'dwxnfeeu'  # Usuário do banco
-database = 'dwxnfeeu'  # Nome do banco
-host = 'tuffi.db.elephantsql.com'  # Host do banco
-password = 'EYsro5jAqVKp9hnoUgX_G8Tw3IjMd9Jo'  # Senha do banco
+
+user = 'dwxnfeeu'
+database = 'dwxnfeeu'
+host = 'tuffi.db.elephantsql.com'
+password = 'EYsro5jAqVKp9hnoUgX_G8Tw3IjMd9Jo'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{user}:{password}@{host}/{database}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "chaveOK"
 
-# Cria o objeto SQL Alchemy, passando o app como parametro e vinculando a variavel db.
-db = SQLAlchemy(app)
 
-# Primeiras informações que vão aparecer no front(index)
-# Id, Nome, Tipo, Nivel de Desafio e Imagem.
+db = SQLAlchemy(app)
 
 
 class Bestas(db.Model):
